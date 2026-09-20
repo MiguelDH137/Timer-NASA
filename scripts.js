2e0c3d235759f9d689fde61521fdd13bce247d8b
DazaihHollow <bungouseven@gmail.com>
2026-09-20 17:07:54 -0400

    feat: CSS styles were created

--- scripts.js
+++ scripts.js
@@ -7,20 +7,20 @@ const timerButton = document.querySelector('#timer-button');
 
 // ========== event listener ========== //
 timerButton.addEventListener('click', () => {
-    let second = secondInput.value || 0;
-    let minute = minuteInput.value || 0;
-    let hour = hourInput.value || 0;
+    let second = parseInt(secondInput.value) || 0;
+    let minute = parseInt(minuteInput.value) || 0;
+    let hour = parseInt(hourInput.value) || 0;
     
     const temporizador = setInterval(() => {
         timerCardData.innerHTML = `${hour}:${minute}:${second}`
 
-        hour = (second == 0 && minute == 0) ? hour - 1 : hour;
+        hour = (second == 0 && minute == 0 && hour > 0) ? hour - 1 : hour;
         minute = (second !== 0) ? minute : (minute > 0) ? minute - 1 : 59;
         second = (second > 0) ? second - 1 : 59
 
         if (hour == 0 && minute == 0 && second == 0) {
             clearInterval(temporizador)
-            timerCardData.innerHTML = `DESPEGUE`
+            timerCardData.innerHTML = `<img src='./assets/rocket.gif' id='gif-despegue'>`
         }
     }, 1000);
 
