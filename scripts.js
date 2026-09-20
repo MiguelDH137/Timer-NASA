a5f6690642975c14a22e077a33bead0c75646ab1
DazaihHollow <bungouseven@gmail.com>
2026-09-20 15:31:02 -0400

    feat: Inputs for minutes and hours were added; logic for a timer for hours, minutes, and seconds was integrated.

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
 // ========== Selectores ========== //
 const timerCardData = document.querySelector('#timer-card-data');
-const timerInput = document.querySelector('#timer-input');
+const secondInput = document.querySelector('#second-input');
+const minuteInput = document.querySelector('#minute-input');
+const hourInput = document.querySelector('#hour-input');
 const timerButton = document.querySelector('#timer-button');
 
 // ========== event listener ========== //
 timerButton.addEventListener('click', () => {
-    let time = timerInput.value;
+    let second = secondInput.value || 0;
+    let minute = minuteInput.value || 0;
+    let hour = hourInput.value || 0;
     
     const temporizador = setInterval(() => {
-        timerCardData.innerHTML = `${time}`
-        time --;
+        timerCardData.innerHTML = `${hour}:${minute}:${second}`
 
-        if (time == -1) {
+        hour = (second == 0 && minute == 0) ? hour - 1 : hour;
+        minute = (second !== 0) ? minute : (minute > 0) ? minute - 1 : 59;
+        second = (second > 0) ? second - 1 : 59
+
+        if (hour == 0 && minute == 0 && second == 0) {
             clearInterval(temporizador)
-            timerCardData.textContent = `DESPEGUE`
+            timerCardData.innerHTML = `DESPEGUE`
         }
     }, 1000);
 
